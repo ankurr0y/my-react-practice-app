9e5c26d8146b9bd8c3925644c9c90e147697c5e7
ankur.roy <ankur.roy@genesesolution.com>
2022-01-08 12:41:50 +0545

    Functionality of X and O

--- src/index.js
+++ src/index.js
@@ -32,12 +32,17 @@ class Board extends React.Component {
         this.state = 
         {      
             squares: Array(9).fill(null),    
+            xIsNext: true,
         };  
     }
     handleClick(i){
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares:squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        console.log(squares);
+        this.setState({
+            squares:squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
     renderSquare(i) {
       return <Square 
@@ -47,11 +52,11 @@ class Board extends React.Component {
     }
   
     render() {
-      const status = 'Next player: X';
+      const game_status = 'Next player: '+(this.state.xIsNext? 'X': 'O');
   
       return (
         <div>
-          <div className="status">{status}</div>
+          <div className="status">{game_status}</div>
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
