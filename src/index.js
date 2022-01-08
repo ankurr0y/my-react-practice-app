2470ce853d626e9bbd73a3353d579f9bd03e72a1
ankur.roy <ankur.roy@genesesolution.com>
2022-01-08 12:53:26 +0545

    function to declare winner

--- src/index.js
+++ src/index.js
@@ -19,6 +19,25 @@ import './index.css';
 //       );
 //     }
 //   }
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 function Square(props) {
     return (
       <button className="square" onClick={props.onClick}>
@@ -52,7 +71,15 @@ class Board extends React.Component {
     }
   
     render() {
-      const game_status = 'Next player: '+(this.state.xIsNext? 'X': 'O');
+    //   const game_status = 'Next player: '+(this.state.xIsNext? 'X': 'O');
+        const winner = calculateWinner(this.state.squares);    
+        let game_status;    
+        if (winner) {      
+            game_status = 'Winner: ' + winner;    
+        }
+        else {      
+            game_status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');    
+        }
   
       return (
         <div>
